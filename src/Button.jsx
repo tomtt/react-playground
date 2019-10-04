fa896b4f402e00cf85d3b0d91b17c4293e712c63
Tom ten Thij <code@tomtenthij.nl>
2019-10-04 11:32:20 +0200

    Move button to its own file
    feature:

---
+++ src/Button.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(() => {
+      return { text: "PROCEED TO CHECKOUT" };
+    });
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.text || this.props.text}
+      </button>
+    );
+  }
+}
+
+export default Button;
--- src/__tests__/Button.spec.js
+++ src/__tests__/Button.spec.js
@@ -1,27 +1,6 @@
 import React from "react";
 import { create } from "react-test-renderer";
-
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: "" };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(() => {
-      return { text: "PROCEED TO CHECKOUT" };
-    });
-  }
-
-  render() {
-    return (
-      <button onClick={this.handleClick}>
-        {this.state.text || this.props.text}
-      </button>
-    );
-  }
-}
+import Button from "../button";
 
 describe("Button component", () => {
   test("it shows the expected text when clicked (testing the wrong way!)", () => {
